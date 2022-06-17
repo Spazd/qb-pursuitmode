8de21b8bf7ce23b8fef90d444b5d6964f58ec447
Jonas Suter <jonas_suter@gmx.ch>
2022-06-18 00:46:12 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,7 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
 
 local gear = 1
-local currentVehicle = 0
+local currentVehicle = nil
 local currentVehicleMode = Config.VehicleModes[1]
 local playerJob = {['name'] = 'unemployed'}
 local hashModelMap = {}
@@ -13,33 +13,61 @@ end
 local function fixVehicleHandling(veh)
     -- Necessary to apply the HandlingFloat
     -- Source: https://forum.cfx.re/t/cant-change-setvehiclehandlingfloat-transforming-vehicle-to-awd-fivem-bug/3393188/2
+
     SetVehicleModKit(veh,0)
+
     SetVehicleMod(veh,0,GetVehicleMod(veh,0),false)
+
     SetVehicleMod(veh,1,GetVehicleMod(veh,1),false)
+
     SetVehicleMod(veh,2,GetVehicleMod(veh,2),false)
+
     SetVehicleMod(veh,3,GetVehicleMod(veh,3),false)
+
     SetVehicleMod(veh,4,GetVehicleMod(veh,4),false)
+
     SetVehicleMod(veh,5,GetVehicleMod(veh,5),false)
+
     SetVehicleMod(veh,6,GetVehicleMod(veh,6),false)
+
     SetVehicleMod(veh,7,GetVehicleMod(veh,7),false)
+
     SetVehicleMod(veh,8,GetVehicleMod(veh,8),false)
+
     SetVehicleMod(veh,9,GetVehicleMod(veh,9),false)
+
     SetVehicleMod(veh,10,GetVehicleMod(veh,10),false)
+
     SetVehicleMod(veh,11,GetVehicleMod(veh,11),false)
+
     SetVehicleMod(veh,12,GetVehicleMod(veh,12),false)
+
     SetVehicleMod(veh,13,GetVehicleMod(veh,13),false)
+
     SetVehicleMod(veh,15,GetVehicleMod(veh,15),false)
+
     SetVehicleMod(veh,16,GetVehicleMod(veh,16),false)
+
     SetVehicleMod(veh,25,GetVehicleMod(veh,25),false)
+
     SetVehicleMod(veh,27,GetVehicleMod(veh,27),false)
+
     SetVehicleMod(veh,28,GetVehicleMod(veh,28),false)
+
     SetVehicleMod(veh,30,GetVehicleMod(veh,30),false)
+
     SetVehicleMod(veh,33,GetVehicleMod(veh,33),false)
+
     SetVehicleMod(veh,34,GetVehicleMod(veh,34),false)
+
     SetVehicleMod(veh,35,GetVehicleMod(veh,35),false)
+
     SetVehicleWheelIsPowered(veh,0,true)
+
     SetVehicleWheelIsPowered(veh,1,true)
+
     SetVehicleWheelIsPowered(veh,2,true)
+
     SetVehicleWheelIsPowered(veh,3,true)
 end
 
@@ -100,12 +128,12 @@ end
 local function applyVehicleMods(vehicle)
     local vehicleMode = Config.VehicleModes[gear]
     
-    ToggleVehicleMod(vehicle, 18, Config["VehicleModifications"][vehicleMode]["Turbo"]) -- Turbo
-    ToggleVehicleMod(vehicle, 22, Config["VehicleModifications"][vehicleMode]["XenonHeadlights"]) -- Xenon Headlights
-    SetVehicleMod(vehicle, 11, Config["VehicleModifications"][vehicleMode]["Engine"], false) -- Engine
-    SetVehicleMod(vehicle, 12, Config["VehicleModifications"][vehicleMode]["Brakes"], false) -- Brakes
-    SetVehicleMod(vehicle, 13, Config["VehicleModifications"][vehicleMode]["Transmission"], false) -- Transmission
-    SetVehicleXenonLightsColour(vehicle, Config["VehicleModifications"][vehicleMode]["XenonHeadlightsColor"]) -- Xenon Headlights Color
+    ToggleVehicleMod(vehicle, 18, Config["VehicleModifications"][vehicleMode]["Turbo"] or GetVehicleMod(vehicle, 18)) -- Turbo
+    ToggleVehicleMod(vehicle, 22, Config["VehicleModifications"][vehicleMode]["XenonHeadlights"] or GetVehicleMod(vehicle, 22)) -- Xenon Headlights
+    SetVehicleMod(vehicle, 11, Config["VehicleModifications"][vehicleMode]["Engine"] or GetVehicleMod(vehicle, 18), false) -- Engine
+    SetVehicleMod(vehicle, 12, Config["VehicleModifications"][vehicleMode]["Brakes"] or GetVehicleMod(vehicle, 18), false) -- Brakes
+    SetVehicleMod(vehicle, 13, Config["VehicleModifications"][vehicleMode]["Transmission"] or GetVehicleMod(vehicle, 18), false) -- Transmission
+    SetVehicleXenonLightsColour(vehicle, Config["VehicleModifications"][vehicleMode]["XenonHeadlightsColor"] or GetVehicleXenonLightsColour(vehicle)) -- Xenon Headlights Color
 
     if Config.SlowdownOnSwitch then
         local speed = GetEntitySpeed(vehicle)
@@ -114,12 +142,18 @@ local function applyVehicleMods(vehicle)
 end
 
 local function changeVehicleMode(vehicle)
-    gear = gear % #Config.VehicleModes + 1
-    if vehicle ~= currentVehicle then
+    local vehicleModel = getModelFromHash(GetEntityModel(vehicle))
+
+    repeat
+        gear = gear % #Config.VehicleModes + 1
+        currentVehicleMode = Config.VehicleModes[gear]
+    until Config.VehiclesConfig[vehicleModel][currentVehicleMode] ~= nil
+
+    if currentVehicle ~= nil and vehicle ~= currentVehicle then
         gear = 1
     end
+
     currentVehicle = vehicle
-    currentVehicleMode = Config.VehicleModes[gear]
     TriggerEvent('qb-pursuitmode:vehicleModeChanged', currentVehicleMode)
 end
 
